ccbb3a33f553dfe074413b4e6315bfe725816c43
despitegfx <despitegfx@gmail.com>
2020-10-08 22:57:10 -0700

    deleted

--- Java/Lab2/Hello.java
+++
@@ -1,8 +0,0 @@
-import course.demo.AnotherClass;
- 
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        AnotherClass.display();
-    }
-}
